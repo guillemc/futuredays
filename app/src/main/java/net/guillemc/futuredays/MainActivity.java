2fe6339bcc4a20fc5ed1281c24a7829bfcc0b529
Guillem Comes <guillemc@gmail.com>
2016-02-11 20:51:33 +0100

    Use a fragment for main activity

--- app/src/main/java/net/guillemc/futuredays/MainActivity.java
+++ app/src/main/java/net/guillemc/futuredays/MainActivity.java
@@ -1,28 +1,15 @@
 package net.guillemc.futuredays;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements ListFragment.Callbacks {
+import net.guillemc.futuredays.helpers.SingleFragmentActivity;
+
+public class MainActivity extends SingleFragmentActivity implements ListFragment.Callbacks {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.tabs_and_pager);
-
-        // Get the ViewPager and set it's PagerAdapter so that it can display items
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new MyFragmentPagerAdapter(getSupportFragmentManager()));
-
-        // Give the TabLayout the ViewPager
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
-        tabLayout.setupWithViewPager(viewPager);
+    protected Fragment createFragment() {
+        return new PagerFragment();
     }
 
     @Override
@@ -37,26 +24,5 @@ public class MainActivity extends AppCompatActivity implements ListFragment.Call
         startActivity(intent);
     }
 
-    private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
-        final int PAGE_COUNT = 2;
 
-        public MyFragmentPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public int getCount() {
-            return PAGE_COUNT;
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return ListFragment.newInstance(position == 0 ? true : false);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return getResources().getString(position == 0 ? R.string.future_items : R.string.past_items);
-        }
-    }
 }
---
+++ app/src/main/java/net/guillemc/futuredays/PagerFragment.java
@@ -0,0 +1,55 @@
+package net.guillemc.futuredays;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PagerFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tabs_and_pager, container, false);
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        viewPager.setAdapter(new MyFragmentPagerAdapter(getActivity().getSupportFragmentManager()));
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return view;
+    }
+
+    private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+        final int PAGE_COUNT = 2;
+
+        public MyFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return ListFragment.newInstance(position == 0 ? true : false);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return getResources().getString(position == 0 ? R.string.future_items : R.string.past_items);
+        }
+    }
+
+}
